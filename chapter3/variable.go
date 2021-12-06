d22aa9bbd6cad86ce6e59bf3c6507263b0c98d57
Thet Khine <mrthetkhine@gmail.com>
2021-12-06 20:58:07 +0630

    Operator

---
+++ chapter3/variable.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	x = 3
+	fmt.Println("hello ", x)
+
+	//x = "Hello"
+}
---
+++ chapter4/division.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = 5
+	b := 2
+
+	fmt.Println("hello ", a/b)
+
+	//x = "Hello"
+}
